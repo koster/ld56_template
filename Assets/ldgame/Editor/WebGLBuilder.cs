36aa611e70ed23ea931757192f428a03b33cdc2f
xkthename <itiskoster@gmail.com>
2024-10-04 13:48:08 +0100

    WebGLBuilder

---
+++ Assets/ldgame/Editor/WebGLBuilder.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+
+public class WebGLBuilder
+{
+    public static void Build()
+    {
+        string[] scenes = { "Assets/ldgame/main.unity" }; // Define your scenes here
+        BuildPipeline.BuildPlayer(scenes, "path_to_output_directory", BuildTarget.WebGL, BuildOptions.None);
+    }
+}
