9f5cd1b7a8ff7c9c22f3af5a6ed676d3b115e94d
xkthename <itiskoster@gmail.com>
2024-10-04 13:52:52 +0100

    dummy

--- Assets/ldgame/Editor/WebGLBuilder.cs
+++ Assets/ldgame/Editor/WebGLBuilder.cs
@@ -4,6 +4,7 @@ public class WebGLBuilder
 {
     public static void Build()
     {
+        // 1
         string[] scenes = { "Assets/ldgame/main.unity" }; // Define your scenes here
         BuildPipeline.BuildPlayer(scenes, "path_to_output_directory", BuildTarget.WebGL, BuildOptions.None);
     }
