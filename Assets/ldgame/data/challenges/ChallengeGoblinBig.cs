fed088c7c6ee54c27765350122bab1346849b51c
xkthename <itiskoster@gmail.com>
2024-10-05 18:39:43 +0100

    fix big goblin

--- Assets/ldgame/data/challenges/ChallengeGoblinBig.cs
+++ Assets/ldgame/data/challenges/ChallengeGoblinBig.cs
@@ -5,7 +5,7 @@ public class ChallengeGoblinBig : CMSEntity
         Define<TagPrefab>().prefab = "prefab/challenges/goblin_big".Load<InteractiveObject>();
         Define<TagPreferSlot>().idx = 1;
         Define<TagChallengeDefinition>().goals.Add(new ChallengeGoalDefinition() { type = GoalType.VALUE, goalValue = 6});
-        Define<TagChallengeDefinition>().goals.Add(new ChallengeGoalDefinition() { type = GoalType.SINK });
+        Define<TagChallengeDefinition>().goals.Add(new ChallengeGoalDefinition() { type = GoalType.SINK, goalValue = 20 });
         Define<TagChallengePenalty>().damage = 3;
     }
 }
