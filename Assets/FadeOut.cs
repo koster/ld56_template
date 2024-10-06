afbe79b312a6a2d5d2ccdd72df4200657e91bad6
xkthename <itiskoster@gmail.com>
2024-10-06 23:53:32 +0100

    fix end screen

---
+++ Assets/FadeOut.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class FadeOut : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        G.fader.FadeOut();
+    }
+}
